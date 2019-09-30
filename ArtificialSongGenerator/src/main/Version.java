8e0c4427aeb3a9b3f754881ebfe40663f0a95737
Fabian Ostermann <fabian.ostermann@udo.edu>
2019-09-30 13:51:42 +0200

    ignore database folders, add OnsetAnnotator version jar

--- ArtificialSongGenerator/src/main/Version.java
+++ ArtificialSongGenerator/src/main/Version.java
@@ -1,8 +1,1 @@
-package main;
-
-public class Version {
-	
-	/** last date this unit was changed: 'version no-yyyy/mm/dd' */
-	public static final String VERSION = "v0.1-2019/09/26";
-
-}
+package main; public class Version { public static final String VERSION = "v0.1-2019/09/30"; }
