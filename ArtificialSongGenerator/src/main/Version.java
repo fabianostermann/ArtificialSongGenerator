ccbf3b685ff1de543db981ac0f5bd948fa25f677
OysterSandwich <info@fabianos.de>
2020-02-18 11:02:49 +0100

    added SamplingRoboter to makeall script (maybe move this script to top direction someday)

--- ArtificialSongGenerator/src/main/Version.java
+++ ArtificialSongGenerator/src/main/Version.java
@@ -1,1 +1,1 @@
-package main; public class Version { public static final String VERSION = "v0.1-2020/02/18-11:01"; }
+package main; public class Version { public static final String VERSION = "v0.1-2020/02/18-11:02"; }
