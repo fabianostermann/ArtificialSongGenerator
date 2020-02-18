e0ad005a217e365bde4b9bcf7f743039a8d80456
OysterSandwich <info@fabianos.de>
2020-02-18 11:01:01 +0100

    added instrument example file

--- ArtificialSongGenerator/src/main/Version.java
+++ ArtificialSongGenerator/src/main/Version.java
@@ -1,1 +1,1 @@
-package main; public class Version { public static final String VERSION = "v0.1-2020/02/17-12:29"; }
+package main; public class Version { public static final String VERSION = "v0.1-2020/02/18-11:01"; }
