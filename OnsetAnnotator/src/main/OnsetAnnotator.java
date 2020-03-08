644f990e6dee7073d8b16f1fba86b8be332e4b24
OysterSandwich <info@fabianos.de>
2020-03-08 19:32:17 +0100

    fixed onsetannotator now working with multiple tempo markers (used java sound midi sequencer module for microsecond conversion)

--- OnsetAnnotator/src/main/OnsetAnnotator.java
+++ OnsetAnnotator/src/main/OnsetAnnotator.java
@@ -11,11 +11,12 @@ import java.util.HashMap;
 import java.util.List;
 
 import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MetaMessage;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
@@ -50,8 +51,9 @@ public class OnsetAnnotator {
     //public static final int NOTE_OFF = 0x80;
 	public static final int SET_TEMPO = 0x51;
 
-	public static int resolution;
-	public static int tempo; // in BPM (beats per minute)
+//	public static int resolution;
+//	public static int tempo; // in BPM (beats per minute)
+	public static Sequencer sequencer;
 
 	public static String[] instrumentOnChannel;
 
@@ -85,6 +87,8 @@ public class OnsetAnnotator {
 		arffToMidiInstrumentMap.put("Organ", "Rock_Organ");
 		arffToMidiInstrumentMap.put("Sitar", "Sitar");
 		arffToMidiInstrumentMap.put("Cello", "String_Ensemble_1");
+		arffToMidiInstrumentMap.put("AcousticGuitar", "Guitar");
+		arffToMidiInstrumentMap.put("ElectricGuitar", "Overdriven_Guitar"+OR+"Electric_Clean_Guitar");
 		arffToMidiInstrumentMap.put("Bass", "Acoustic_Bass"+OR+"Contrabass");
 		arffToMidiInstrumentMap.put("ElectricBass", "Electric_Bass_Finger"+OR+"Slap_Bass_1");
 		arffToMidiInstrumentMap.put("Drums", "Drums");
@@ -122,17 +126,17 @@ public class OnsetAnnotator {
 
 		System.out.println("Analysing '"+fileName+".mid'..");
 		
-        // check division type
-        if (sequence.getDivisionType() != Sequence.PPQ) {
-          System.out.println("Wrong devision type. Need PPQ, got "+ sequence.getDivisionType());
-          System.exit(1);
-        }
-        // get resolution setting (normally 128 ticks per quarter note)
-        resolution = sequence.getResolution();
+//        // check division type
+//        if (sequence.getDivisionType() != Sequence.PPQ) {
+//          System.out.println("Wrong devision type. Need PPQ, got "+ sequence.getDivisionType());
+//          System.exit(1);
+//        }
+//        // get resolution setting (normally 128 ticks per quarter note)
+//        resolution = sequence.getResolution();
         
         // initiate tick2second calculation
         initTickToSecond(sequence);
-        System.out.println("Read tempo successfully (set tempo="+tempo+"bpm)");
+        //System.out.println("Read tempo successfully (set tempo="+tempo+"bpm)");
 
         // read in channel instrument mapping
         instrumentOnChannel = new String[16];
@@ -267,33 +271,47 @@ public class OnsetAnnotator {
         System.out.println("arff file '"+arffFile.getName()+"' written.");
     }
     
+//    public static void initTickToSecond(Sequence sequence) {
+//    	// tempo is needed in track 0 at position @0 for now (TODO use tempo map later)
+//    	Track track = sequence.getTracks()[0]; 
+//    	for (int i=0; i < track.size(); i++) { 
+//            MidiEvent event = track.get(i);
+//            if (event.getTick() > 0) {
+//            	System.out.println("Set_Tempo event not found it track:0-tick:0");
+//            	System.out.println(1);
+//            }
+//            MidiMessage message = event.getMessage();
+//            if (message instanceof MetaMessage) {
+//            	MetaMessage mm = (MetaMessage) message;
+//            	if(mm.getType()==SET_TEMPO){
+//            		byte[] data = mm.getData();
+//            		int midiTempo = (data[0] & 0xff) << 16 | (data[1] & 0xff) << 8 | (data[2] & 0xff);
+//            		int tempoInBPM = 60000000 / midiTempo;
+//            		tempo = tempoInBPM;
+//                }
+//            	return;
+//            }
+//    	}
+//    }
+    
+
     public static void initTickToSecond(Sequence sequence) {
-    	// tempo is needed in track 0 at position @0 for now (TODO use tempo map later)
-    	Track track = sequence.getTracks()[0]; 
-    	for (int i=0; i < track.size(); i++) { 
-            MidiEvent event = track.get(i);
-            if (event.getTick() > 0) {
-            	System.out.println("Set_Tempo event not found it track:0-tick:0");
-            	System.out.println(1);
-            }
-            MidiMessage message = event.getMessage();
-            if (message instanceof MetaMessage) {
-            	MetaMessage mm = (MetaMessage) message;
-            	if(mm.getType()==SET_TEMPO){
-            		byte[] data = mm.getData();
-            		int midiTempo = (data[0] & 0xff) << 16 | (data[1] & 0xff) << 8 | (data[2] & 0xff);
-            		int tempoInBPM = 60000000 / midiTempo;
-            		tempo = tempoInBPM;
-                }
-            	return;
-            }
-    	}
+    	// init midi system
+    	try {
+			sequencer = MidiSystem.getSequencer();
+	    	sequencer.open();
+	    	sequencer.setSequence(sequence);
+    	} catch (MidiUnavailableException | InvalidMidiDataException e) {
+			System.out.println("Error while initiating midi sequencer: "+e.getMessage());
+			System.exit(1);
+		}
     }
     
-    // TODO make tickToSecond() useful with multiple tempo change marks!
     // see: https://stackoverflow.com/questions/23070510/how-to-get-exact-time-of-a-midi-event
     public static float tickToSecond(long tick) {
-    	return (float)(tick*60)/(tempo*resolution); // (tick / resolution) * (60 / tempo)
+//    	return (float)(tick*60)/(tempo*resolution); // (tick / resolution) * (60 / tempo)
+    	sequencer.setTickPosition(tick);
+    	return ((float) sequencer.getMicrosecondPosition()) / 1000000.f;
     }
     
     static String[] noteNames = { "C", "Cis", "D", "Dis", "E", "F", "Fis", "G",
