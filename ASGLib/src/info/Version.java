d10a11ec9fbe25344003f300b64def833d5f33f2
OysterSandwich <info@fabianos.de>
2020-03-08 19:33:03 +0100

    rearranged main build file

--- ASGLib/src/info/Version.java
+++ ASGLib/src/info/Version.java
@@ -1,1 +1,1 @@
-package info; public class Version { public static final String VERSION = "v0.1-2020/03/08-19:32"; }
+package info; public class Version { public static final String VERSION = "v0.1-2020/03/08-19:33"; }
