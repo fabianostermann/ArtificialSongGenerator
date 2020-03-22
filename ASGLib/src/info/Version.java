5a94e64412e25bc8c69c14de2cb4d7338be9f7b7
Fabian Ostermann <fabian.ostermann@udo.edu>
2020-03-22 16:18:29 +0100

    added run target to ant build file

--- ASGLib/src/info/Version.java
+++ ASGLib/src/info/Version.java
@@ -1,1 +1,1 @@
-package info; public class Version { public static final String VERSION = "v0.1-2020/03/21-22:13"; }
+package info; public class Version { public static final String VERSION = "v0.1-2020/03/22-16:18"; }
